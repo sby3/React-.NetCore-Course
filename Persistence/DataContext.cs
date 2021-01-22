7a128ba6fd4dc4e2915eeecb0477dc9c38a108d6
Stephen Byrd <skbyrd@protonmail.com>
2021-01-21 20:02:32 -0600

    Adding additional code

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        protected DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values {get; set; }
+    }
+}
